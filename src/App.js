e15cfd7744f4aa7122726387c80f915d8b84033a
xheartean <info@gokhankoc.pl>
2020-07-19 20:20:39 +0200

    done

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to re load.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          You are a bird
-        </a>
-      </header>
+      <header className="App-header">You are a bird</header>
     </div>
   );
 }
